0af9881868fa3248e767d1f954a3f703b58d6c62
ZakharTsarapov <tsarapovs@gmail.com>
2023-06-28 19:21:09 +0300

    chakra ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,20 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export const App = () => {
  
   
   return (
-    <div>
-      <h1 className={css.text}>Phonebook</h1>
-      <ContactForm />
-      <h2 className={css.text}>Contacts</h2>
-      <Filter />
-      <ContactsList/>
-    </div>
+    <ChakraProvider>
+      <div>
+        <h1 className={css.text}>Phonebook</h1>
+        <ContactForm />
+        <h2 className={css.text}>Contacts</h2>
+        <Filter />
+        <ContactsList />
+      </div>
+    </ChakraProvider>
   );
 };
